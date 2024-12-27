ec0513f02813ee4226c09ca909a0bde802085e4e
simy46 <missipsa.annane@polymtl.ca>
2024-12-27 03:29:59 -0500

    feat:can now compile and run basic 10sec on/off LED

--- Arduino Test/src/main.cpp
+++ Arduino Test/src/main.cpp
@@ -1,18 +1,15 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT); // Configure la LED intégrée comme sortie
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Allumer la LED intégrée
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(10000); // Attend 10 secondes (10 000 ms)
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Éteindre la LED intégrée
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(10000); // Attend 10 secondes (10 000 ms)
 }
